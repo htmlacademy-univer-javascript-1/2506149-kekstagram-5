8ac5f6c459bc8e32ff6a48c1b14b20bdcfd9d792
Самойлова Кристина <samojlovakristina036@mail.ru>
2024-11-10 14:53:57 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,12 @@
+/* eslint-disable no-unused-vars */
 function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
-
-console.log(checkStringLength('проверяемая строка', 20));
-console.log(checkStringLength('проверяемая строка', 18));
-console.log(checkStringLength('проверяемая строка', 10));
-
 
 function isPalindrom(str) {
   str = str.toLowerCase();
   return str === str.split('').reverse().join('');
 }
-
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
 
 function extractNumber(arg){
   const string = arg.toString();
@@ -27,11 +19,12 @@ function extractNumber(arg){
   return parseInt(res, 10);
 }
 
-console.log(extractNumber('2023 год'));
-console.log(extractNumber('ECMAScript 2022'));
-console.log(extractNumber('1 кефир, 0.5 батона'));
-console.log(extractNumber('агент 007'));
-console.log(extractNumber('а я томат'));
-console.log(extractNumber(2023));
-console.log(extractNumber(-1));
-console.log(extractNumber(1.5));
+function isMeetingWithinWorkHours(workStart, workEnd, meetingStart, durationMinutes) {
+  function timeToMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+  const meetingEndMinutes = timeToMinutes(meetingStart) + durationMinutes;
+
+  return timeToMinutes(workStart) <= timeToMinutes(meetingStart) && meetingEndMinutes <= timeToMinutes(workEnd);
+}
